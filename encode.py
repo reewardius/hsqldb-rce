0308116188a1dbd186573f4775e5b6edc0d4d5b4
DIMOOON <68978608+reewardius@users.noreply.github.com>
2023-03-30 03:22:35 +0300

    Update encode.py

--- encode.py
+++ encode.py
@@ -6,6 +6,6 @@ def encode_file_to_varbinary(file_path):
         varbinary_content = binascii.hexlify(file_content)
         return "CAST('{}' AS VARBINARY(1024))".format(varbinary_content.decode())
 
-file_path = 'myjsp.jsp'
+file_path = 'shell.jsp'
 varbinary_str = encode_file_to_varbinary(file_path)
 print(varbinary_str)
